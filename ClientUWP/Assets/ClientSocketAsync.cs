ce06794546f648fad59dc3c0eecb83b05351cd3c
Nader Nour <nnmh1992@hotmail.com>
2017-01-09 16:44:07 +0300

    First Working Demo

---
+++ ClientUWP/Assets/ClientSocketAsync.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+[Serializable]
+public class ObjectToTransfer
+{
+    public int PlayerID;
+    public int ObjectID;
+    public byte[] TextureRawData;
+    public int Width;
+    public int Height;
+    public TextureFormat Format;
+    public bool mipmap;
+    public ObjectToTransfer(int _PlayerID, int _ObjectID, Texture2D ScannedTexture)
+    {
+        PlayerID = _PlayerID;
+        ObjectID = _ObjectID;
+        TextureRawData = ScannedTexture.GetRawTextureData();
+        Width = ScannedTexture.width;
+        Height = ScannedTexture.height;
+        Format = ScannedTexture.format;
+        mipmap = ScannedTexture.mipmapCount > 1;
+    }
+
+    public Texture2D GetTexture()
+    {
+        Texture2D OutputTexture = new Texture2D(Width, Height, Format, mipmap);
+        OutputTexture.LoadRawTextureData(TextureRawData);
+        OutputTexture.Apply();
+        return OutputTexture;
+    }
+}
+public class ClientSocketAsync : MonoBehaviour {
+    public Text outputText;
+    public InputField inputText;
+    public string ServerIp = "172.16.1.103";
+    public Texture2D texToSend;
+    // Cached Socket object that will be used by each call for the lifetime of this class
+    Socket _socket = null;
+
+    // Signaling object used to notify when an asynchronous operation is completed
+    static ManualResetEvent _clientDone = new ManualResetEvent(false);
+
+    // Define a timeout in milliseconds for each asynchronous call. If a response is not received within this 
+    // timeout period, the call is aborted.
+    const int TIMEOUT_MILLISECONDS = 5000;
+
+    // The maximum size of the data buffer to use with the asynchronous socket methods
+    const int MAX_BUFFER_SIZE = 1024;
+
+    public int portNumber = 3003;
+
+    static string text = "";
+    ObjectToTransfer obj;
+    private void Start()
+    {
+        obj = new ObjectToTransfer(4, 5, texToSend);
+       // Connect();
+    }
+    private void Update()
+    {
+        outputText.text = text;
+    }
+
+
+    /// <summary>
+    /// Attempt a TCP socket connection to the given host over the given port
+    /// </summary>
+    /// <param name="hostName">The name of the host</param>
+    /// <param name="portNumber">The port number to connect</param>
+    /// <returns>A string representing the result of this connection attempt</returns>
+    public void Connect()
+    {
+        string result = string.Empty;
+
+        // Create DnsEndPoint. The hostName and port are passed in to this method.
+        ServerIp = inputText.text;
+        IPEndPoint hostEntry = new IPEndPoint(IPAddress.Parse(ServerIp), portNumber);
+
+        // Create a stream-based, TCP socket using the InterNetwork Address Family. 
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Create a SocketAsyncEventArgs object to be used in the connection request
+        SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+        socketEventArg.RemoteEndPoint = hostEntry;
+
+        // Inline event handler for the Completed event.
+        // Note: This event handler was implemented inline in order to make this method self-contained.
+        socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate (object s, SocketAsyncEventArgs e)
+        {
+            // Retrieve the result of this request
+            result = e.SocketError.ToString();
+
+            print(result);
+            text += result;
+            // Signal that the request is complete, unblocking the UI thread
+            _clientDone.Set();
+            //print("Connected");
+            if(result == "Success")
+            {
+
+                Send();
+            }
+
+        });
+
+        // Sets the state of the event to nonsignaled, causing threads to block
+        _clientDone.Reset();
+
+        // Make an asynchronous Connect request over the socket
+        _socket.ConnectAsync(socketEventArg);
+
+        // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+        // If no response comes back within this time then proceed
+        _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+    }
+
+    /// <summary>
+    /// Send the given data to the server using the established connection
+    /// </summary>
+    /// <param name="data">The data to send to the server</param>
+    /// <returns>The result of the Send request</returns>
+    public string Send()
+    {
+        string response = "Operation Timeout";
+
+        // We are re-using the _socket object initialized in the Connect method
+        if (_socket != null)
+        {
+            // Create SocketAsyncEventArgs context object
+            SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+
+            // Set properties on context object
+            socketEventArg.RemoteEndPoint = _socket.RemoteEndPoint;
+            socketEventArg.UserToken = null;
+
+            // Inline event handler for the Completed event.
+            // Note: This event handler was implemented inline in order 
+            // to make this method self-contained.
+            socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate (object s, SocketAsyncEventArgs e)
+            {
+                response = e.SocketError.ToString();
+
+                // Unblock the UI thread
+                _clientDone.Set();
+            });
+
+            // Add the data to be sent into the buffer
+
+
+            // Prepare the package and send it.
+            byte[] Data = ObjectToByteArray(obj);
+            //byte[] Data = { 1, 2, 10, 4, 5, 6 };
+            byte[] Package = new byte[4 + Data.Length];
+            byte[] dataLength = BitConverter.GetBytes(Data.Length);
+            dataLength.CopyTo(Package, 0);
+            Data.CopyTo(Package, 4);
+
+            socketEventArg.SetBuffer(Package, 0, Package.Length);
+
+            // Sets the state of the event to nonsignaled, causing threads to block
+            _clientDone.Reset();
+
+            // Make an asynchronous Send request over the socket
+            _socket.SendAsync(socketEventArg);
+
+            // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+            // If no response comes back within this time then proceed
+            _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+        }
+        else
+        {
+            response = "Socket is not initialized";
+        }
+
+        return response;
+    }
+
+
+    public string Receive()
+    {
+        string response = "Operation Timeout";
+
+        // We are receiving over an established socket connection
+        if (_socket != null)
+        {
+            // Create SocketAsyncEventArgs context object
+            SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+            socketEventArg.RemoteEndPoint = _socket.RemoteEndPoint;
+
+            // Setup the buffer to receive the data
+            socketEventArg.SetBuffer(new Byte[MAX_BUFFER_SIZE], 0, MAX_BUFFER_SIZE);
+
+            // Inline event handler for the Completed event.
+            // Note: This even handler was implemented inline in order to make 
+            // this method self-contained.
+            socketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(delegate (object s, SocketAsyncEventArgs e)
+            {
+                if (e.SocketError == SocketError.Success)
+                {
+                    // Retrieve the data from the buffer
+                    response = Encoding.UTF8.GetString(e.Buffer, e.Offset, e.BytesTransferred);
+                    response = response.Trim('\0');
+                }
+                else
+                {
+                    response = e.SocketError.ToString();
+                }
+
+                _clientDone.Set();
+            });
+
+            // Sets the state of the event to nonsignaled, causing threads to block
+            _clientDone.Reset();
+
+            // Make an asynchronous Receive request over the socket
+            _socket.ReceiveAsync(socketEventArg);
+
+            // Block the UI thread for a maximum of TIMEOUT_MILLISECONDS milliseconds.
+            // If no response comes back within this time then proceed
+            _clientDone.WaitOne(TIMEOUT_MILLISECONDS);
+        }
+        else
+        {
+            response = "Socket is not initialized";
+        }
+
+        return response;
+    }
+
+
+    public static byte[] ObjectToByteArray(ObjectToTransfer obj)
+    {
+        return Encoding.UTF8.GetBytes(JsonUtility.ToJson(obj));
+    }
+
+    public static ObjectToTransfer ByteArrayToObject(byte[] arrBytes)
+    {
+        return JsonUtility.FromJson<ObjectToTransfer>(ASCIIEncoding.UTF8.GetString(arrBytes));
+    }
+}
---
+++ ServerUWP/Assets/ServerSocketAsync.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+[Serializable]
+public class ObjectToTransfer
+{
+    public int PlayerID;
+    public int ObjectID;
+    public byte[] TextureRawData;
+    public int Width;
+    public int Height;
+    public TextureFormat Format;
+    public bool mipmap;
+    public ObjectToTransfer(int _PlayerID, int _ObjectID, Texture2D ScannedTexture)
+    {
+        PlayerID = _PlayerID;
+        ObjectID = _ObjectID;
+        TextureRawData = ScannedTexture.GetRawTextureData();
+        Width = ScannedTexture.width;
+        Height = ScannedTexture.height;
+        Format = ScannedTexture.format;
+        mipmap = ScannedTexture.mipmapCount > 1;
+    }
+
+    public Texture2D GetTexture()
+    {
+        Texture2D OutputTexture = new Texture2D(Width, Height, Format, mipmap);
+        OutputTexture.LoadRawTextureData(TextureRawData);
+        OutputTexture.Apply();
+        return OutputTexture;
+    }
+}
+public class StateObject
+{
+    // Client  socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 1024;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public byte[] Data;
+
+    public int DataLength = -1;
+
+    public int BytesLeft = 0;
+
+    public int BytesRead = 0;
+}
+
+
+public class ServerSocketAsync : MonoBehaviour
+{
+    public Text testText;
+    static string test = "";
+
+    public static int PortNum = 3003;
+
+    public RawImage image;
+    // Thread signal.
+    public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+    static ObjectToTransfer obj;
+    static Socket listener;
+    private void Start()
+    {
+        //RecieveThread = new Thread(StartListening);
+        //RecieveThread.Start();
+        StartListening();
+    }
+
+    void Update()
+    {
+        if (obj != null)
+        {
+            image.texture = obj.GetTexture();
+        }
+        testText.text = test;
+    }
+
+    public static void StartListening()
+    {
+
+        // Establish the local endpoint for the socket.
+        // The DNS name of the computer
+        // running the listener is "host.contoso.com".
+        test += "" + (GetIPAddress());
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, PortNum);
+
+        // Create a TCP/IP socket.
+        listener = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and listen for incoming connections.
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
+            // Set the event to nonsignaled state.
+            allDone.Reset();
+            listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+            //while (true)
+            //{
+
+
+            //    // Start an asynchronous socket to listen for connections.
+            //    print("Waiting for a connection...");
+
+
+            //    // Wait until a connection is made before continuing.
+            //    allDone.WaitOne();
+            //}
+
+        }
+        catch (Exception e)
+        {
+            test += (e.ToString());
+        }
+    }
+
+    public static void AcceptCallback(IAsyncResult ar)
+    {
+        // Signal the main thread to continue.
+        allDone.Set();
+
+        // Get the socket that handles the client request.
+        Socket listener = (Socket)ar.AsyncState;
+        Socket handler = listener.EndAccept(ar);
+
+        // Create the state object.
+        StateObject state = new StateObject();
+        state.workSocket = handler;
+        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+            new AsyncCallback(ReadCallback), state);
+    }
+
+    public static void ReadCallback(IAsyncResult ar)
+    {
+        // Retrieve the state object and the handler socket
+        // from the asynchronous state object.
+        StateObject state = (StateObject)ar.AsyncState;
+        Socket handler = state.workSocket;
+
+        // Read data from the client socket. 
+        int bytesRead = handler.EndReceive(ar);
+        if (bytesRead > 0)
+        {
+            if (state.DataLength <= 0)
+            {
+                state.DataLength = BitConverter.ToInt32(state.buffer, 0);
+                state.BytesLeft = state.DataLength + 4 - bytesRead;
+                state.Data = new byte[state.DataLength];
+                Array.Copy(state.buffer, 4, state.Data, 0, bytesRead - 4);
+                state.BytesRead = bytesRead - 4;
+                if (state.BytesLeft > 0)
+                {
+                    // Not all data received. Get more.
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+                else
+                {
+                    ConstructObj(state.Data);
+                }
+            }
+            else
+            {
+                // There  might be more data, so store the data received so far
+                Array.Copy(state.buffer, 0, state.Data, state.BytesRead, bytesRead);
+                state.BytesRead += bytesRead;
+                state.BytesLeft -= bytesRead;
+                if (state.BytesLeft > 0)
+                {
+                    // Not all data received. Get more.
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+                else
+                {
+                    // All the data has been read from the 
+                    // client. Display it on the console.
+                    ConstructObj(state.Data);
+                    // Echo the thanks back to the client.
+                    // Send(handler, content);
+                }
+            }
+        }
+        else
+        {
+            test += ("WTF");
+        }
+    }
+
+    static void ConstructObj(byte[] Data)
+    {
+        //print("Read " + Data.Length + " bytes from socket. \n Data :");
+        //test += (Data.Length);
+        //test += (Data[1]);
+        //test += (Data[3]);
+        listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+        obj = JsonUtility.FromJson<ObjectToTransfer>(Encoding.UTF8.GetString(Data));
+    }
+
+    private static void Send(Socket handler, String data)
+    {
+        // Convert the string data to byte data using ASCII encoding.
+        byte[] byteData = Encoding.UTF8.GetBytes(data);
+
+        // Begin sending the data to the remote device.
+        handler.BeginSend(byteData, 0, byteData.Length, 0,
+            new AsyncCallback(SendCallback), handler);
+    }
+
+    private static void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            // Retrieve the socket from the state object.
+            Socket handler = (Socket)ar.AsyncState;
+
+            // Complete sending the data to the remote device.
+            int bytesSent = handler.EndSend(ar);
+            print("Sent" + bytesSent + "bytes to client.");
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+        }
+        catch (Exception e)
+        {
+            print(e.ToString());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // RecieveThread.Abort();
+    }
+
+
+    public static string GetIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("Local IP Address Not Found!");
+    }
+}
